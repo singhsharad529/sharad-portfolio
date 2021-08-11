b939125a7005e53d0262e3d0f005ea6c3a12fd16
Sharad Kumar Singh <singhsharad529@gmail.com>
2021-08-11 18:24:33 +0530

    addedd contact and footer

--- script.js
+++ script.js
@@ -5,12 +5,39 @@ $(document).ready(function () {
     } else {
       $(".navbar").removeClass("sticky");
     }
+
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+  });
+
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+    // removing smooth scroll on slide-up button click
+    $("html").css("scrollBehavior", "auto");
   });
 
   // toggle menu/navbar script
   $(".menu-btn").click(function () {
     $(".navbar .menu").toggleClass("active");
     $(".menu-btn i").toggleClass("active");
+  });
+
+  // typing animation script
+  var typed = new Typed(".typing", {
+    strings: ["Developer", "Programmer"],
+    typeSpeed: 110,
+    backSpeed: 60,
+    loop: true,
+  });
+
+  var typed = new Typed(".typing-2", {
+    strings: ["Developer", "Programmer"],
+    typeSpeed: 110,
+    backSpeed: 60,
+    loop: true,
   });
 
   // owl carousel script
